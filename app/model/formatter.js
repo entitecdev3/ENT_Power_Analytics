b441dc095fd4ff366185848a3c6173d4ead78eb9
yashsoyuztechnologies <yash@soyuztechnologies.com>
2025-06-04 10:37:14 +0530

    Report Add and Update part without odata

--- app/model/formatter.js
+++ app/model/formatter.js
@@ -6,39 +6,24 @@ sap.ui.define([], function () {
       if (!roles || roles.length === 0) return "";
       return roles.map(r => r.name).join(", ");
     },
-    formatCompanyDisplay: async function (ID) {
-      const oModel = this.getView().getModel();
-      const oBinding = oModel.bindList("/Companies");
-
-      const aContexts = await oBinding.requestContexts(0, 1000); // fetch up to 1000 companies
-      const aData = aContexts.map(oContext => oContext.getObject());
-      let oCompany = aData.find((company) => company.ID === ID);
+    formatCompanyDisplay: function (ID, aData) {
+      let oCompany = aData?.find((company) => company.ID === ID);
       if (oCompany) {
         return oCompany.name;
       }
       return "";
     },
-    formatRoleDisplay: async function (ID) {
-      const oModel = this.getView().getModel();
-      const oBinding = oModel.bindList("/Roles");
-
-      const aContexts = await oBinding.requestContexts(0, 1000); // fetch up to 1000 companies
-      const aData = aContexts.map(oContext => oContext.getObject());
-      let oRoles = aData.find((role) => role.ID === ID);
+    formatRoleDisplay: function (ID, aData) {
+      let oRoles = aData?.find((role) => role.ID === ID);
       if (oRoles) {
         return oRoles.name;
       }
       return "";
     },
-    formatDisplaySetting : async function (ID) {
-      const oModel = this.getView().getModel();
-      const oBinding = oModel.bindList("/DisplaySettings");
-
-      const aContexts = await oBinding.requestContexts(0, 1000); // fetch up to 1000 display settings
-      const aData = aContexts.map(oContext => oContext.getObject());
-      let oSetting = aData.find((setting) => setting.ID === ID);
-      if (oSetting) {
-        return `${oSetting.property} :${oSetting.value}`;
+    formatServicePrincipal: function (ID, aData) {
+      let oData = aData?.find((role) => role.ID === ID);
+      if (oData) {
+        return oData.biUser;
       }
       return "";
     },
